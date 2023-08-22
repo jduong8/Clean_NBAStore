8a05fa0bce78ccdb387cfb612852ee5adfc66935
Duong Jonathan <djonathan91.77@gmail.com>
2023-08-22 13:09:48 +0200

    add product type mapper

---
+++ Clean_NBAStore/Domain/Mappers/ProductType+Mappers.swift
@@ -0,0 +1,25 @@
+//
+//  ProductType+Mappers.swift
+//  Clean_NBAStore
+//
+//  Created by Jonathan Duong on 22/08/2023.
+//
+
+import Foundation
+
+extension ProductType {
+    init(with dtoType: ProductTypeDTO) throws {
+        switch dtoType {
+        case .tshirt:
+            self = .tshirt
+        case .hat:
+            self = .hat
+        case .pants:
+            self = .pants
+        case .hoodie:
+            self = .hoodie
+        case .short:
+            self = .short
+        }
+    }
+}
