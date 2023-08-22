c370bb402488206785ff24dcf389d322f510fe45
Duong Jonathan <djonathan91.77@gmail.com>
2023-08-22 10:10:35 +0200

    Initial NBA Store project

---
+++ Clean_NBAStore/Clean_NBAStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_NBAStoreApp.swift
+//  Clean_NBAStore
+//
+//  Created by Jonathan Duong on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clean_NBAStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
